66c7dba314f6e4413c90a55e28969786141ac593
Charles_Luk <792281365@qq.com>
2020-11-06 23:14:26 +0800

    1、更新：公共组件topTabBar删除“scan”logo、更换LOGO为图片

--- src/components/topTabBar/topTabBar.jsx
+++ src/components/topTabBar/topTabBar.jsx
@@ -3,9 +3,8 @@ import React, { Component } from 'react'
 import { View, Text, Input, Image } from '@tarojs/components'
 import './topTabBar.less'
 
-import msg_img from './assets/msg.png'
 import search_img from './assets/search.png'
-import scan_img from './assets/scan.png'
+import logo from '../../assets/logo.jpeg'
 
 export default class TabBar extends Component {
 
@@ -25,11 +24,12 @@ export default class TabBar extends Component {
   render() {
     return (
       <View className='tabBar'>
-        <View className='logo' onClick={this.toIndex}>FOCUS</View>
+        <View className='logo' onClick={this.toIndex}>
+          <Image src={logo} mode='widthFix' />
+        </View>
         <View className='search_bar'>
           <Image src={search_img} className='search_img' />
           <Input placeholder='输入课程/活动/场地' confirm-type='search' onConfirm={this.handleSearchOk} />
-          <Image src={scan_img} className='scan_img' />
         </View>
       </View>
     )
